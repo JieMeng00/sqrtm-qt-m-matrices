1ed0ecd32537a6c329496cdce3e4bf1d26467c9d
jiemeng <105709485+JieMeng00@users.noreply.github.com>
2022-06-02 10:58:06 +0800

    Add files via upload

---
+++ crsroot.m
@@ -0,0 +1,35 @@
+function [G,r] = crsroot(A)
+% function G = crsroot(A) 
+% Compute the square root of quasi-Toeplitz M-matrix A by Cyclic reduction
+verb = true; maxit = 1000; epsi = 1.e-14;  cqtoption('threshold',10^(-15))
+I = cqt( 1, 1 );
+Y = I - A;
+Z = 2 * (I + A);
+err= 1;
+r = zeros( maxit, 1 );
+for k=1:maxit
+    Yold = Y;
+    Zold = Z;
+    
+    Y    = -Yold * Zold^(-1) * Yold;
+    Z    =  Zold + 2 * Y;
+    
+    errold = err;
+    err    = norm( Z^2 - 16 * A, inf );
+    
+    
+    
+    
+    if err < epsi || (err >= errold && k > 1), break; end
+    
+    if verb 
+      fprintf('step=%d, err=%d\n',k,err); 
+    end
+     
+     r(k)   = err;
+end
+G = 1/4 * Z;
+
+if (k == maxit)
+fprintf('Warning: reached the max number of iterations');
+end
